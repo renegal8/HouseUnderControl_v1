afe90c99e2807c2fe275635b892f26be86786369
ACmeza <acmf3094@hotmail.com>
2015-03-30 02:50:20 -0600

    Revert "FALTA ENVIAR IMAGEN"
    This reverts commit 3a6cab895f7ec9d529ad1e22638cde6567e36451.

--- app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/activity_config.java
+++ app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/activity_config.java
@@ -6,27 +6,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class activity_config extends ActionBarActivity {
-    String LOG_TAG = "ConfigurarActivity";
+
     String[] cuartos = new String[]{};
     ArrayAdapter<String> adapterCuartos;
-    Button guardarBtn;
-    TextView cantCuartosTV;
-    EditText nombreET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +27,12 @@ public class activity_config extends ActionBarActivity {
         setContentView(R.layout.activity_activity_config);
 
         ListView cuartosLV = (ListView) findViewById(R.id.cuartosLV);
-        cantCuartosTV = (TextView) findViewById(R.id.cantCuartosValueTV);
-        nombreET = (EditText)findViewById(R.id.nombreValueET);
-
-        guardarBtn = (Button) findViewById(R.id.botonGuardar);
 
         AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String cantidad = ((TextView)view).getText().toString();
-                if(cantidad=="1"){
-                    cantCuartosTV.setText("1");}
-                else if(cantidad=="2"){
-                    cantCuartosTV.setText("2");}
-                else if(cantidad=="3"){
-                    cantCuartosTV.setText("3");}
-                else if(cantidad=="4"){
-                    cantCuartosTV.setText("4");}
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+
 
             }
         };
@@ -58,7 +40,7 @@ public class activity_config extends ActionBarActivity {
         cuartosLV.setOnItemClickListener(itemListener);
 
         cuartos = new String[]{
-                "1", "2", "3", "4"
+                "1 Cuarto", "2 Cuartos", "3 Cuartos", "4 Cuartos"
         };
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.activity_row, R.id.rowTV, cuartos);
@@ -68,32 +50,42 @@ public class activity_config extends ActionBarActivity {
         ImageView img= (ImageView) findViewById(R.id.casaIV);
         img.setImageResource(R.mipmap.casa1);
 
+        Button guardarBtn = (Button) findViewById(R.id.guardarBtn);
 
         guardarBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
 
+                Intent intent = new Intent(activity_config.this, MainActivity.class);
+                //                       intent.putExtra("email", emailET.getText().toString());
+                //                       intent.putExtra("password", passwordET.getText().toString());
 
-                if(nombreET.getText().toString().trim().matches("")|| cantCuartosTV.getText().toString().trim().matches(""))
-                {
-                    Toast.makeText(getApplicationContext(), "Faltan Datos por Llenar", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    try {
-                        Intent returnIntent = new Intent();
-                        returnIntent.putExtra("name", nombreET.getText().toString());
-                        returnIntent.putExtra("cantidad", cantCuartosTV.getText().toString());
-                        //returnIntent.putExtra("sistema", sistemaTV.getText().toString());
-                        setResult(RESULT_OK, returnIntent);
-                        Toast.makeText(getApplicationContext(), "Configuracion Correcta", Toast.LENGTH_SHORT).show();
-                        finish();
-                    } catch (Exception e) {
-                        Log.e(LOG_TAG, "Failed to send intent", e);
-                    }
-                }
+                startActivity(intent);
+
             }
         });
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_config, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
