3a6cab895f7ec9d529ad1e22638cde6567e36451
ACmeza <acmf3094@hotmail.com>
2015-03-30 02:48:25 -0600

    FALTA ENVIAR IMAGEN

--- app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/MainActivity.java
+++ app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/MainActivity.java
@@ -20,51 +20,57 @@ import java.util.Arrays;
 
 public class MainActivity extends ActionBarActivity {
 
+    String LOG_TAG = "MainActivity";
+    Button config;
+    Button verconfig;
+    String tname;
+    String tcant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button config = (Button) findViewById(R.id.configBtn);
+        config = (Button) findViewById(R.id.boton_configurar);
+        verconfig = (Button) findViewById(R.id.boton_verConfiguracion);
 
         config.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
 
-                        Intent intent = new Intent(MainActivity.this, activity_config.class);
- //                       intent.putExtra("email", emailET.getText().toString());
- //                       intent.putExtra("password", passwordET.getText().toString());
+                try {
+                    Intent intent = new Intent(MainActivity.this, activity_config.class);
+                    startActivityForResult(intent, 1);
+                }
+                catch(Exception e){
+                    Log.e(LOG_TAG, "Failed to send intent", e);
+                }
+            }
+        });
 
-                        startActivity(intent);
-
+        verconfig.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, ver_configuracion.class);
+                    intent.putExtra("nombre",tname);
+                    intent.putExtra("cantidad",tcant);
+                    startActivity(intent);
+                }
+                catch(Exception e){
+                    Log.e(LOG_TAG, "Failed to send intent", e);
+                }
             }
         });
 
 
 
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            tname = data.getStringExtra("name");
+            tcant = data.getStringExtra("cantidad");
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/ver_configuracion.java
+++ app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/ver_configuracion.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class ver_configuracion extends ActionBarActivity {
@@ -12,28 +13,17 @@ public class ver_configuracion extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_configuracion);
+
+        TextView nombreTV = (TextView) findViewById(R.id.nombreValueTV);
+        TextView cantidadTV = (TextView) findViewById(R.id.cantidadValueTV);
+
+        Bundle bundle = getIntent().getExtras();
+        String n = bundle.getString("nombre");
+        String c =bundle.getString("cantidad");
+
+        nombreTV.setText(n);
+        cantidadTV.setText(c);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ver_configuracion, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
