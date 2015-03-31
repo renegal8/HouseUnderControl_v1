d4a65803e19c9dcd197d14b7a91a5c80b572b48e
renegalvez <rene_gd8@hotmail.com>
2015-03-30 21:19:58 -0600

    mostrar imagen en ver config

--- app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/ver_configuracion.java
+++ app/src/main/java/holamundo/itesm/mx/houseundercontrol_v1/ver_configuracion.java
@@ -1,18 +1,20 @@
 package holamundo.itesm.mx.houseundercontrol_v1;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class ver_configuracion extends ActionBarActivity {
 
     ImageView casaIV;
-    Bitmap imagenBP;
+    Bitmap imageBP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +28,27 @@ public class ver_configuracion extends ActionBarActivity {
         Bundle bundle = getIntent().getExtras();
         String n = bundle.getString("nombre");
         String c =bundle.getString("cantidad");
-        imagenBP = (Bitmap) bundle.getParcelable("photo");
-        casaIV.setImageBitmap(imagenBP);
+//        imagenBP = (Bitmap) bundle.getParcelable("photo");
+//        casaIV.setImageBitmap(imagenBP);
+        String photo = bundle.getString("photo");
 
         nombreTV.setText(n);
         cantidadTV.setText(c);
+
+        if(photo.equals("1")){
+            imageBP = BitmapFactory.decodeResource(getResources(), R.mipmap.casa1);
+            casaIV.setImageBitmap(imageBP);
+        } else if(photo.equals("2")){
+            imageBP = BitmapFactory.decodeResource(getResources(),R.mipmap.casa2);
+            casaIV.setImageBitmap(imageBP);
+        } else if(photo.equals("3")){
+            imageBP = BitmapFactory.decodeResource(getResources(),R.mipmap.casa3);
+            casaIV.setImageBitmap(imageBP);
+        } else if(photo.equals("4")){
+            imageBP = BitmapFactory.decodeResource(getResources(),R.mipmap.casa4);
+            casaIV.setImageBitmap(imageBP);
+        }
+//
 
     }
 
